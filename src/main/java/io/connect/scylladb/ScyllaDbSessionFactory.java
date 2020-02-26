bf9dbb85a7077c93d65fce6573c70b69378e949d
patitapaban19 <patitapaban.mohapatra@hashedin.com>
2020-02-26 15:16:35 +0530

    error handling and ssl config support

--- src/main/java/io/connect/scylladb/ScyllaDbSessionFactory.java
+++ src/main/java/io/connect/scylladb/ScyllaDbSessionFactory.java
@@ -15,14 +15,19 @@ import org.apache.kafka.connect.errors.ConnectException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.TrustManagerFactory;
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
 public class ScyllaDbSessionFactory {
@@ -59,31 +64,55 @@ public class ScyllaDbSessionFactory {
 
       if (null != config.trustStorePath) {
         log.info("Configuring SSLContext to use Truststore {}", config.trustStorePath);
-        final KeyStore keyStore;
-        try {
-          keyStore = KeyStore.getInstance("JKS");
-          try (InputStream inputStream = new FileInputStream(config.trustStorePath)) {
-            keyStore.load(inputStream, config.trustStorePassword);
-          } catch (IOException e) {
-            throw new ConnectException("Exception while reading keystore", e);
-          } catch (CertificateException | NoSuchAlgorithmException e) {
-            throw new ConnectException("Exception while loading keystore", e);
-          }
-        } catch (KeyStoreException e) {
-          throw new ConnectException("Exception while creating keystore", e);
-        }
+        final KeyStore trustKeyStore = createKeyStore(config.trustStorePath, config.trustStorePassword);
 
         final TrustManagerFactory trustManagerFactory;
         try {
           trustManagerFactory =
               TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-          trustManagerFactory.init(keyStore);
+          trustManagerFactory.init(trustKeyStore);
         } catch (NoSuchAlgorithmException e) {
           throw new ConnectException("Exception while creating TrustManagerFactory", e);
         } catch (KeyStoreException e) {
           throw new ConnectException("Exception while calling TrustManagerFactory.init()", e);
         }
         sslContextBuilder.trustManager(trustManagerFactory);
+      }
+
+      if (null != config.keyStorePath) {
+        log.info("Configuring SSLContext to use Keystore {}", config.keyStorePath);
+        final KeyStore keyStore = createKeyStore(config.keyStorePath, config.keyStorePassword);
+
+        final KeyManagerFactory keyManagerFactory;
+        try {
+          keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+          keyManagerFactory.init(keyStore, config.keyStorePassword);
+        } catch (NoSuchAlgorithmException e) {
+          throw new ConnectException("Exception while creating KeyManagerFactory", e);
+        } catch (UnrecoverableKeyException | KeyStoreException e) {
+          throw new ConnectException("Exception while calling KeyManagerFactory.init()", e);
+        }
+        sslContextBuilder.keyManager(keyManagerFactory);
+      }
+
+      if (config.cipherSuites.size() > 0) {
+        sslContextBuilder.ciphers(config.cipherSuites);
+      }
+
+      if (config.certFilePath != null && config.privateKeyPath != null) {
+        try {
+          sslContextBuilder.keyManager(new BufferedInputStream(new FileInputStream(config.certFilePath)),
+                  new BufferedInputStream(new FileInputStream(config.privateKeyPath)));
+        }
+        catch (IllegalArgumentException e) {
+          throw new ConnectException(String.format("Invalid certificate or private key: %s", e.getMessage()));
+        } catch (FileNotFoundException e) {
+          throw new ConnectException("Invalid certificate or private key file path", e);
+        }
+      } else if (config.certFilePath == null != (config.privateKeyPath == null)) {
+        throw new ConnectException(String.format("%s cannot be set without %s and vice-versa: %s is not set",
+                "scylladb.ssl.openssl.keyCertChain", "scylladb.ssl.openssl.privateKey",
+                (config.certFilePath == null) ? "scylladb.ssl.openssl.keyCertChain" : "scylladb.ssl.openssl.privateKey"));
       }
 
       final SslContext context;
@@ -100,5 +129,22 @@ public class ScyllaDbSessionFactory {
     log.info("Creating session");
     final Session session = cluster.newSession();
     return new ScyllaDbSessionImpl(config, cluster, session);
+  }
+
+  private KeyStore createKeyStore(File path, char[] password) {
+    KeyStore keyStore;
+    try {
+      keyStore = KeyStore.getInstance("JKS");
+      try (InputStream inputStream = new FileInputStream(path)) {
+        keyStore.load(inputStream, password);
+      } catch (IOException e) {
+        throw new ConnectException("Exception while reading keystore", e);
+      } catch (CertificateException | NoSuchAlgorithmException e) {
+        throw new ConnectException("Exception while loading keystore", e);
+      }
+    } catch (KeyStoreException e) {
+      throw new ConnectException("Exception while creating keystore", e);
+    }
+    return keyStore;
   }
 }
