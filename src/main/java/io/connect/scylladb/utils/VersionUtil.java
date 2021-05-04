2b846fecf949dc319f225f2660d98ed1dfb73e08
Piotr Grabowski <piotr.grabowski@scylladb.com>
2021-05-04 14:45:40 +0200

    Fix VersionUtil::getVersion()
    Fix VersionUtil::getVersion(). The new way the version is loaded is
    inspired by Debezium connectors and Scylla CDC Source Connector.
    
    The version is stored in "version.properties" resource file and Maven
    on build-time fills this file with a version number. VersionUtil
    can then read it in runtime, by loading the properties file.
    
    Fixes #39.

--- src/main/java/io/connect/scylladb/utils/VersionUtil.java
+++ src/main/java/io/connect/scylladb/utils/VersionUtil.java
@@ -1,11 +1,29 @@
 package io.connect.scylladb.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class VersionUtil {
-    public static String getVersion() {
+    private static final Logger log = LoggerFactory.getLogger(VersionUtil.class);
+    private static final Properties VERSION = loadVersion();
+
+    private static Properties loadVersion() {
+        // Load the version from version.properties resource file.
+        InputStream inputStream = VersionUtil.class.getClassLoader().getResourceAsStream("io/connect/scylladb/version.properties");
+        Properties props = new Properties();
         try {
-            return VersionUtil.class.getPackage().getImplementationVersion();
-        } catch (Exception ex) {
-            return "0.0.0.0";
+            props.load(inputStream);
+        } catch (IOException e) {
+            log.error("Error loading the connector version");
         }
+        return props;
+    }
+
+    public static String getVersion() {
+        return VERSION.getProperty("version");
     }
 }
